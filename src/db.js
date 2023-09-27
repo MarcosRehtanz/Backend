633a6bd61df48671a757c240a4d2dece0edc29a8
GabiTom <gabrieltomasi22@gmail.com>
2023-09-26 22:45:51 -0300

    feat(src/db.js): add relations between models

--- src/db.js
+++ src/db.js
@@ -1,5 +1,6 @@
 import { Sequelize } from "sequelize";
 import 'dotenv/config'
+
 import { user } from "./Models/User.js";
 import { product } from "./Models/Product.js";
 import { cart } from "./Models/Cart.js";
@@ -8,6 +9,8 @@ import { typePerson } from "./Models/TypePerson.js";
 import { typeUser } from "./Models/TypeUser.js";
 
 const sequelize = new Sequelize( process.env.POSTGRES, {logging:false} )
+
+
 
 //Define
 user(sequelize)
@@ -18,8 +21,28 @@ typePerson(sequelize)
 typeUser(sequelize)
 
 //Models
-const { User, Product } = sequelize.models
-console.log(User, Product)
+const { User, Product, Cart, ShoppingHistory,  TypePerson, TypeUser} = sequelize.models
+console.log(sequelize.models)
+
+
+// Relations
+
+User.hasOne(Cart);
+Cart.belongsTo(User);
+
+Product.belongsToMany(Cart, {through: 'ProductCart'})
+Cart.belongsToMany(Product, {through:'ProductCart'})
+
+User.hasMany(ShoppingHistory,{
+    foreignKey: 'userID'
+  })
+ShoppingHistory.belongsTo(User)
+
+User.hasOne(TypePerson);
+TypePerson.belongsTo(User);
+
+User.belongsToMany(TypeUser, {through: 'UserTypeUser'})
+TypeUser.belongsToMany(User, {through:'UserTypeUser'})
 
 export const conn = sequelize
 export const models = sequelize.models
