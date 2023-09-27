7598e05ca78f0300bd5f0fa707a8bf091082d79d
danielyepez13 <danielyepez62@gmail.com>
2023-09-27 16:35:35 -0400

    feat(graphql/query): changes sortering

--- src/Query/orderingPrice.js
+++ src/Query/orderingPrice.js
@@ -2,41 +2,42 @@ import { models } from "../db.js"
 
 
 export const orderingPrice = async (root, args) => {
-    const { orderPrice, orderStock  } = args
+    const { orderPrice, orderStock } = args
 
     try {
         const product = await models.Product.findAll();
-        if (orderStock){
-            if (orderStock === "ASC") {
+        if (orderStock) {
+            if (orderPrice === "ASC") {
                 product.sort((a, b) => {
-                    if (a.stock < b.stock) return 1
-                    if (b.stock < a.stock) return -1
-                    if (a.stock === b.stock) {
-                        return (orderPrice === "ASC") ? a.price - b.price : b.price - a.price;
+                    if (a.price < b.price) return -1
+                    if (b.price < a.price) return 1
+                    if (a.price === b.price) {
+                        return (orderStock === "ASC") ? a.stock - b.stock : b.stock - a.stock;
                     }
                     return 0;
                 });
             } else {
                 product.sort((a, b) => {
-                    if (a.stock < b.stock) return -1
-                    if (b.stock < a.stock) return 1
-                    if (a.stock === b.stock) {
-                        return (orderPrice === "ASC") ? a.price - b.price : b.price - a.price;
+                    if (a.price < b.price) return 1
+                    if (b.price < a.price) return -1
+                    if (a.price === b.price) {
+                        return (orderStock === "ASC") ? a.stock - b.stock : b.stock - a.stock;
                     }
                     return 0;
                 });
             }
+        } else {
+            if (orderPrice === "ASC") {
+                product.sort((a, b) => a.price - b.price)
+            } else {
+                product.sort((a, b) => b.price - a.price)
+            }
         }
 
-        if (orderPrice === "ASC") {
-            product.sort((a,b)=> a.price - b.price)
-        } else {
-            product.sort((a,b) => b.price - a.price)
-        }
 
         return product
     } catch (error) {
-        
+
     }
 
 }
--- src/Query/orderingStock.js
+++ src/Query/orderingStock.js
@@ -7,32 +7,33 @@ export const orderingStock = async (root, args) => {
     try {
         const product = await models.Product.findAll();
         if (orderPrice){
-            if (orderPrice === "ASC") {
+            if (orderStock === "ASC") {
                 product.sort((a, b) => {
-                    if (a.price < b.price) return 1
-                    if (b.price < a.price) return -1
-                    if (a.price === b.price) {
-                        return (orderStock === "ASC") ? a.stock - b.stock : b.stock - a.stock;
+                    if (a.stock < b.stock) return -1
+                    if (b.stock < a.stock) return 1
+                    if (a.stock === b.stock) {
+                        return (orderPrice === "ASC") ? a.price - b.price : b.price - a.price;
                     }
                     return 0;
                 });
             } else {
                 product.sort((a, b) => {
-                    if (a.price < b.price) return -1
-                    if (b.price < a.price) return 1
-                    if (a.price === b.price) {
-                        return (orderStock === "ASC") ? a.stock - b.stock : b.stock - a.stock;
+                    if (a.stock < b.stock) return 1
+                    if (b.stock < a.stock) return -1
+                    if (a.stock === b.stock) {
+                        return (orderPrice === "ASC") ? a.price - b.price : b.price - a.price;
                     }
                     return 0;
                 });
             }
+        } else{
+            if(orderStock === "ASC") {
+                product.sort((a,b)=> a.stock - b.stock)
+            } else {
+                product.sort((a,b) => b.stock - a.stock)
+            }
         }
 
-        if(orderStock === "ASC") {
-            product.sort((a,b)=> a.stock - b.stock)
-        } else {
-            product.sort((a,b) => b.stock - a.stock)
-        }
 
         return product
     } catch (error) {
