2219a0ad5467b3cb36e1f1ba458ad84537b0f102
Jonnier <jonniersalaverria@gmail.com>
2023-09-28 16:44:44 -0500

    feat(Mutation) add file uploadProductImg

--- src/Mutation/Mutation.js
+++ src/Mutation/Mutation.js
@@ -1,7 +1,7 @@
 import { gql } from "apollo-server";
 import { addUser } from "./addUser.js";
 import { addProduct } from "./addProduct.js";
-
+import { uploadProductImg } from "./uploadProductImg.js";
 import { addShoppingHistory } from "./addShoppingHistory.js";
 // import { addTypePerson } from "./addTypePerson.js";
 import { addTypeUser } from "./addTypeUser.js";
@@ -45,7 +45,7 @@ export const MutationType = gql`
             typeUser: String
         ): TypeUser
         addMaterial(name: String! origen:String!): Material 
-        
+        uploadProductImg(photo: String): String
         deleteProduct(id: ID): String
     }
 `
@@ -58,4 +58,5 @@ export const Mutation = {
     // addTypePerson,
     addTypeUser,
     addMaterial,
+    uploadProductImg
 }
