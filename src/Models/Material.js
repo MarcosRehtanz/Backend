e09bcb9aaaa46bc0c8bfbf16a2109e5bdfef835b
Jonnier <jonniersalaverria@gmail.com>
2023-09-29 14:52:57 -0500

    feat(Material): changed origen by origin

--- src/Models/Material.js
+++ src/Models/Material.js
@@ -11,7 +11,7 @@ export const material = (sequelize) =>{
             type: DataTypes.STRING,
             allowNull: true,
         },
-        origen:{
+        origin:{
             type: DataTypes.STRING,
             allowNull: true,
         },
