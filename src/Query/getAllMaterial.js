baf0a7eb3df26ee259b9a129b6d67ec3ee5fb3ee
danielyepez13 <danielyepez62@gmail.com>
2023-09-30 21:42:31 -0400

    feat(minimos): se cambio el nombre del asc al ordenar stock

--- src/Query/getAllMaterial.js
+++ src/Query/getAllMaterial.js
@@ -3,7 +3,6 @@ import { models } from "../db.js"
 export const getAllMaterial = async()=>{
     try {
        const material = await models.Material.findAll()
-       console.log(material);
        if(!material) return new Error ("No hay materiales en la base de datos")
        return material 
     } catch (error) {
