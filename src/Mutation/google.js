5db363b40526d85ed5ecd02c9b6bcda9e5070266
GabiTom <gabrieltomasi22@gmail.com>
2023-10-02 12:01:24 -0300

    fix(google.js)refact google.js

--- src/Mutation/google.js
+++ src/Mutation/google.js
@@ -4,20 +4,20 @@ export const google = async (root, args) => {
   const { email, name, lastname } = args;
   if (!email || !name || !lastname) throw new Error("faltan datos");
 
-  const userGoogle = await models.User.findOne({
-    where: { email },
-  });
-  if(userGoogle) return userGoogle.dataValues
-
-  const createUserWithGoogle = await models.User.create({
+  const createUserWithGoogle = await models.User.findOrCreate({where:{
     email, name, lastname
-  }) 
+  }}) 
 
-  if(!createUserWithGoogle) throw new Error ("No se registro al usuario en la base de datos");
+  if(!createUserWithGoogle) return ("usuario ya creado anteriormente");
 
-  
+  const res = {
+    idUser: createUserWithGoogle[0].dataValues.idUser,
+    name: createUserWithGoogle[0].dataValues.name,
+    email:createUserWithGoogle[0].dataValues.email,
+    lastname:createUserWithGoogle[0].dataValues.lastname
+  }
 
-  return createUserWithGoogle.dataValues
+  return res
   
 
 };
