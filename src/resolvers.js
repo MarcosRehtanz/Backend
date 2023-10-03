e04ae75a7b0a4ac532d9d67e8eeb2f2007e9dfd3
danielyepez13 <danielyepez62@gmail.com>
2023-10-03 19:44:42 -0400

    feat(reviews): added averageRating in product schema

--- src/resolvers.js
+++ src/resolvers.js
@@ -3,5 +3,12 @@ import { Mutation } from "./Mutation/Mutation.js"
 
 export const resolvers = {
     Query,
+    Product: {
+        averageRating: (root) => {
+            const totalReview = root.Reviews.length
+            const totalRating = root.Reviews.reduce((acc, obj) => acc + obj.rating, 0);
+            return Math.floor(totalRating / totalReview)
+        }
+    },
     Mutation,
 }
