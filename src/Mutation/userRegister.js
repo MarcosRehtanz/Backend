b5d1ed221dbef06184dc6297bfce2de75f91c1f3
Mansilla <marcos.rehtanz@gmail.com>
2023-10-04 00:01:06 -0300

    feat(Mutation): add recibe token

--- src/Mutation/userRegister.js
+++ src/Mutation/userRegister.js
@@ -4,14 +4,14 @@ import 'dotenv/config'
 
 export const userRegister = async (_, args) => {
     const { GMAIL } = process.env
-    const token = jwt.sign(args, '123')
+    const token = jwt.sign(args, process.env.JWT_PRIVATE_KEY)
 
     await transporter.sendMail({
         from: `Message from <${GMAIL}>`,
         to: `${args.email}`,
         html: `
         <h1>this is your token:</h1>
-        <a href="http://localhost:3000/${token}" ><button>Verify</button></a>
+        <a href="http://localhost:5173/verify?key=${token}" ><button>Verify</button></a>
         `
     })
     return { ...args, token }
