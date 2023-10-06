fca15e3eec6ebb202a76eacd6daf5226a5d76ad8
danielyepez13 <danielyepez62@gmail.com>
2023-10-06 16:52:19 -0400

    feat(index): data preload

--- index.js
+++ index.js
@@ -25,12 +25,16 @@ conn.sync({ force: true }).then(async () => {
   const materialsId = material.map(id => id[0].dataValues.id)
 
   // Hago un nuevo recorrido pero ahora de los materiales que dependen de los primeros
-  await Promise.all(SubMaterials.map(async (sub, index) => await models.SubMaterials.findOrCreate({
+  const subMaterial = await Promise.all(SubMaterials.map(async (sub, index) => await models.SubMaterials.findOrCreate({
     where: {
       ...sub,
       MaterialId: materialsId[index]
     }
   })))
+
+  const subMaterialsId = subMaterial.map(sub => (
+    { id: sub[0].dataValues.id, materialId: sub[0].dataValues.MaterialId }
+  ))
 
   // Recorrido para hacer usuarios
   const userss = await Promise.all(Users.map(async (user, i) => {
@@ -57,12 +61,21 @@ conn.sync({ force: true }).then(async () => {
       }
     })
 
-    await Promise.all(Products.map(async p => await models.Product.findOrCreate({
-      where: {
+    await Promise.all(Products.map(async p => {
+      const product = await models.Product.create({
         ...p,
         UserIdUser: users[0].dataValues.idUser
-      }
-    })))
+      })
+      // Relacion aleatoria de cada producto con un o dos materiales
+      const randomMaterial = Math.floor(Math.random() * materialsId.length)
+      const random2Material = Math.floor(Math.random() * materialsId.length)
+      if (random2Material !== randomMaterial) await product.addMaterials([materialsId[randomMaterial], materialsId[random2Material]])
+      else await product.addMaterials([materialsId[randomMaterial]])
+    
+      const randomSubMaterial = subMaterialsId.find(sub => sub.materialId === materialsId[randomMaterial])
+      await product.addSubMaterials([randomSubMaterial.id])
+    }
+    ))
   }))
 
   try {
