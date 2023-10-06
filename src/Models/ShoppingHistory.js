a254b95418abbb2b3d5c57581a833fe5cf8465c0
GabiTom <gabrieltomasi22@gmail.com>
2023-10-06 14:37:51 -0300

    feat(ShoppingHistory): modify model, mutation and schema shoppinghistory

--- src/Models/ShoppingHistory.js
+++ src/Models/ShoppingHistory.js
@@ -1,19 +1,30 @@
 import { DataTypes } from "sequelize";
 
-export const shoppingHistory = (sequelize) =>{
-    sequelize.define('ShoppingHistory', {
-        IDShopHistory: {
-            type: DataTypes.UUID,
-            primaryKey: true,
-            defaultValue: DataTypes.UUIDV4
-        },
-        billDate: {
-            type: DataTypes.DATEONLY,
-            allowNull: false
-        },
-        totalAmount: {
-            type: DataTypes.DOUBLE,
-            allowNull: false
-        }
-    });
+export const shoppingHistory = (sequelize) => {
+  sequelize.define("ShoppingHistory", {
+    IDShopHistory: {
+      type: DataTypes.UUID,
+      primaryKey: true,
+      defaultValue: DataTypes.UUIDV4,
+    },
+    operationId: {
+      type: DataTypes.INTEGER,
+    },
+    paymentMethod: {
+      type: DataTypes.STRING,
+    },
+    paymentMethodId: {
+      type: DataTypes.STRING,
+    },
+    netAmount: {
+      type: DataTypes.FLOAT,
+    },
+    taxes: {
+      type: DataTypes.FLOAT,
+    },
+    totalAmount: {
+      type: DataTypes.DOUBLE,
+      allowNull: false,
+    },
+  });
 };
