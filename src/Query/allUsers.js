bf0a78ad72f2c13eab2a6939e889d9bbeec3be4d
GabiTom <gabrieltomasi22@gmail.com>
2023-10-06 22:17:28 -0300

    fix (allUsers.js) : structure response

--- src/Query/allUsers.js
+++ src/Query/allUsers.js
@@ -10,9 +10,28 @@ export const allUsers = async() => {
                 include: models.Profile
             }
         )
-        if(!users) throw new Error (error.message)
-  
-        return users
+        if(!users) throw new Error ("No se encontraron usuarios")
+
+        const AllUsers = users.map(usuario =>{
+            //console.log(usuario.dataValues)
+            return {
+                    idUser: usuario.dataValues.idUser,
+                    name: usuario.dataValues.name,
+                    lastname: usuario.dataValues.lastname,
+                    email:usuario.dataValues.email,
+                    password: usuario.dataValues.password,
+                    accountActive: usuario.dataValues.accountActive,
+                    termsAndCondsAprove: usuario.dataValues.termsAndCondsAprove,
+                    createdAt: String(usuario.dataValues.createdAt),
+                    updatedAt: String(usuario.dataValues.updatedAt),
+                    deletedAt: usuario.dataValues.deletedAt,
+                    profile: usuario.dataValues.Profile.dataValues
+                }
+                
+            }
+        )
+    
+        return AllUsers
 
     } catch (error) {
         throw new Error (error.message)
