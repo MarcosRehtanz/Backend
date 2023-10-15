ddebea9e050373a2f7c3ef0367f5e5386e002327
Jonnier <jonniersalaverria@gmail.com>
2023-10-14 22:54:49 -0500

    fix(Query/allShoppingHistory.js): changed the structure

--- src/Query/allShoppingHistory.js
+++ src/Query/allShoppingHistory.js
@@ -4,8 +4,13 @@ import { models } from "../db.js"
 export const allShoppingHistory = async() => {
     
     try {
-        const shoppingHistory = await models.ShoppingHistory.findAll()
-        return shoppingHistory
+        const shoppingHistory = await models.ShoppingHistory.findAll({include: models.BuyOrders})
+        // console.log(shoppingHistory[0].dataValues.BuyOrders)
+        return shoppingHistory.map(shop => {
+            return {
+                ...shop.dataValues, buyOrders: shop.dataValues.BuyOrders
+            }
+        })
     } catch (error) {
         throw new Error('No se pudo cargar los historiales de compra ' + error.message)
     }
